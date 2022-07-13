7440d5e7f3d35b8c2cf8312c95bcdcdd47ca44cf
Adrián Pelayo <niniohhns@gmail.com>
2022-07-13 15:50:10 +0200

    Styling Register,
    Co-authored-by: Sergiaparicio <sergitus2525@gmail.com>

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -32,9 +32,6 @@ const Login = () => {
     return (
         <div className="flex bg-[url('http://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=80&w=1171')] bg-center bg-no-repeat bg-cover h-screen w-full justify-center items-center">
             <form className="flex flex-col bg-Dark h-3/6  w-72 rounded-3xl justify-evenly px-8 items-center">
-                {errors && (
-                    <div>{errors}</div>
-                )}
                 <p className="text-white font-bold text-3xl">Login <span className="text-History">Solidario</span></p>
                 <div className="flex flex-col gap-6 ">
                     <input required className="h-12 w-48 rounded-full px-6" id="email" type="email" name="email" onChange={(event) => setEmail(event.target.value)} value={email} placeholder="Email..."/>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -35,50 +35,38 @@ const Register = () => {
             {submitted ? 
                 (<Navigate to="/login" />) : 
                 (
-                    <div className="flex bg-[url('https://images.unsplash.com/photo-1600921413222-7d4324c17035?ixlib=rb-1.2.1&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80&w=1171')] bg-no-repeat bg-cover bg-center h-screen w-full justify-center items-center">
-                        <form className="flex flex-col bg-Dark h-4/6  w-72 rounded-3xl justify-evenly px-8 items-center">
+                    <div className="flex bg-[url('https://images.unsplash.com/photo-1600921413222-7d4324c17035?ixlib=rb-1.2.1&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80&w=1171')] bg-no-repeat bg-cover bg-center h-screen w-screen justify-center items-center">
+                        <form className="flex flex-col bg-Dark w-4/5 md:w-2/4 lg:w-2/6 rounded-3xl justify-evenly py-8 gap-10 items-center">
                             <p className="text-white font-bold text-2xl">Register <span className="text-Entertainment">Solidario</span></p>
-                            <div className="flex flex-col gap-6 ">
+                            <div className="flex flex-col gap-6 w-full h-full px-6">
                                 {errors.username && (
-                                    <div className="bg-white">
-                                        <p>Hay errores con el usuario:</p>
-                                        <ul>
-                                            {errors.username.map(error => <li>{error}</li>)}
-                                        </ul>
+                                    <div className="text-white font-bold px-1">
+                                        {errors.username.map(error => <p>{error}</p>)}
                                     </div>
                                 )}
-                                <input required className="h-12 w-48 rounded-full px-6" id="username" name="username" type="text" onChange={(event) => setUsername(event.target.value)} value={username} placeholder="Usuario.."/>
+                                <input required className="rounded-full py-3 px-6" id="username" name="username" type="text" onChange={(event) => setUsername(event.target.value)} value={username} placeholder="Usuario.."/>
                                 {errors.email && (
-                                    <div className="bg-white">
-                                        <p>Hay errores con el correo:</p>
-                                        <ul>
-                                            {errors.email.map(error => <li>{error}</li>)}
-                                        </ul>
+                                    <div className="text-white font-bold px-1">
+                                        {errors.email.map(error => <p>{error}</p>)}
                                     </div>
                                 )}
-                                <input required className="h-12 w-48 rounded-full px-6" id="email" name="email" type="email" onChange={(event) => setEmail(event.target.value)} value={email}  placeholder="Email..."/>
+                                <input required className="rounded-full py-3 px-6" id="email" name="email" type="email" onChange={(event) => setEmail(event.target.value)} value={email}  placeholder="Email..."/>
                                 {errors.password && (
-                                    <div className="bg-white">
-                                        <p>Hay errores con la contraseña:</p>
-                                        <ul>
-                                            {errors.password.map(error => <li>{error}</li>)}
-                                        </ul>
+                                    <div className="text-white font-bold px-1">
+                                        {errors.password.map(error => <p>{error}</p>)}
                                     </div>
                                 )}
-                                <input required className="h-12 w-48 rounded-full px-6" id="password" type="password" name="password" onChange={(event) => setPassword(event.target.value)} value={password}  placeholder="Contraseña.."/>
+                                <input required className="rounded-full py-3 px-6" id="password" type="password" name="password" onChange={(event) => setPassword(event.target.value)} value={password}  placeholder="Contraseña.."/>
                                 {errors["repeat-password"] && (
-                                    <div className="bg-white">
-                                        <p>Hay errores con la contraseña repetida:</p>
-                                        <ul>
-                                            {errors["repeat-password"].map(error => <li>{error}</li>)}
-                                        </ul>
+                                    <div className="text-white font-bold px-1">
+                                        {errors["repeat-password"].map(error => <p>{error}</p>)}
                                     </div>
                                 )}
-                                <input required className="h-12 w-48 rounded-full px-6" id="repeat-password" name="repeat-pasword" type="password" onChange={(event) => setRepeatPassword(event.target.value)} value={repeatPassword} placeholder="Repetir contraseña..."/>
+                                <input required className="rounded-full py-3 px-6" id="repeat-password" name="repeat-pasword" type="password" onChange={(event) => setRepeatPassword(event.target.value)} value={repeatPassword} placeholder="Repetir contraseña..."/>
                             </div>
-                            <button type="submit" onClick={(event) => signUp(event)} className="text-white font-bold text-2xl w-fit px-10 h-12 rounded-full bg-Entertainment">Registrarse</button>
                             <div>
-                                <p className=" text-white font-bold text-lg">¿Ya tienes cuenta?</p>
+                                <button type="submit" onClick={(event) => signUp(event)} className="text-white font-bold text-2xl w-fit px-10 h-12 rounded-full bg-Entertainment mb-2">Registrarse</button>
+                                <p className="text-white font-bold text-lg">¿Ya tienes cuenta?</p>
                                 <p><a href="/Login"><span className="font-bold text-xl text-History">Login</span></a></p>
                             </div>
                         </form>
